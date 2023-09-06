0f937f88680fbef2460d8122a29c928dd5065d2e
akhil-haridas <4khilharidas@gmail.com>
2023-09-06 15:13:42 +0530

    code optimized

--- back-end/server.js
+++ back-end/server.js
@@ -2,29 +2,25 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
 // const morgan = require("morgan");
+const path = require("path");
 const config = require("./config/serverConfig");
-const connectToDatabase = require("./utils/database");
 
-const corsOptions = {
-  origin: [config.server.ORIGIN],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-};
+const connectToDatabase = require("./utils/database");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors(corsOptions));
+app.use(cors());
 // app.use(morgan("dev"));
 
 connectToDatabase();
 
 const userRoutes = require("./routes/userRoutes");
+
 app.use("/api/user", userRoutes);
 
-const PORT = config.server.port
+const PORT = config.server.port;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
